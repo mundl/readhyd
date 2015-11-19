1b6812a0b52eb626d445f79d8a2550ebefc1c6b8
Tobias Gauster <t.gauster@boku.ac.at>
2015-11-19 20:10:57 +0100

    austria: fix encoding for NA values

--- R/austria.R
+++ R/austria.R
@@ -1,6 +1,6 @@
-read.hzb <- function(file, parse.header = TRUE) {
+read.hzb <- function(file, parse.header = TRUE, fileEncoding = "ISO8859-1") {
   # separate the header, open the connection with correct enconding
-  fh <- file(file, open = "rt", encoding = "latin1")
+  fh <- file(file, open = "rt", encoding = fileEncoding)
   header <- readLines(fh, n = 50)
   close(fh)
 
@@ -9,16 +9,20 @@ read.hzb <- function(file, parse.header = TRUE) {
   lines.header <- grep("Werte:", header, fixed = T)
   header <- head(header, lines.header - 1)
 
-  args <- list(file = file, header = F, skip = lines.header, na.strings = "L\u00fccke",
-               strip.white = TRUE, as.is = TRUE, fileEncoding = "latin1")
+  na.strings <- iconv("L\u00fccke", from = "UTF-8", to = fileEncoding)
+
+  args <- list(file = file, header = F, skip = lines.header,
+               na.strings = na.strings, fileEncoding = fileEncoding,
+               strip.white = TRUE, as.is = TRUE)
 
   infile <- if(type == "fwf") {
     do.call(read.fwf, c(args, list(col.names = c("time", "value"),
                                    colClasses = c("character", "numeric"),
                                    widths = c(20, 20))))
   } else if (type == "csv2"){
-    do.call(read.csv2, c(args, list(col.names = c("time", "value", NA),
-                                    colClasses = c("character", "numeric", "NULL"))))
+    do.call(read.csv2, c(args,
+                         list(col.names = c("time", "value", NA),
+                              colClasses = c("character", "numeric", "NULL"))))
   }
 
   infile$time <- as.POSIXct(infile$time, format = "%d.%m.%Y %H:%M:%S")
@@ -53,23 +57,67 @@ read.hzb <- function(file, parse.header = TRUE) {
   keyval <- .split_keyval(keyval)
 
   header.list <-  split(x[which(!is.keyval)], idx[!is.keyval])
-  formatter <- match.fun(paste(".format_list_", type))
-  header.list <- do.call(c, lapply(unname(header.list), formatter))
+
+  # Abschnitt "Exportzeitreihe" is komisch formatiert...
+  header.list <- header.list[!grepl("Exportzeitreihe", header.list)]
+  header.list <- do.call(c, lapply(unname(header.list), .split_list_whitespace))
 
   return(list(keyval = keyval, list = header.list))
 }
 
 
-.split_keyval <- function(x, only.first = T) {
-  # sometimes, only the first ";" is a field separator, replace it with "\t"
-  x <- if(only.first) sub(";", "\t", x) else gsub(";", "\t", x)
-  keyval <- do.call(rbind, strsplit(x, split = "\t"))
-  keyval <- apply(keyval, 2, trimws)
-  keyval <- apply(keyval, 2, sub, pattern = ":$", replacement = "")
+# .split_keyval_csv <- function(x, only.first = T) {
+#   # sometimes, only the first ";" is a field separator, replace it with "\t"
+#   x <- if(only.first) sub(";", "\t", x) else gsub(";", "\t", x)
+#   keyval <- do.call(rbind, strsplit(x, split = "\t"))
+#   keyval <- apply(keyval, 2, trimws)
+#   keyval <- apply(keyval, 2, sub, pattern = ":$", replacement = "")
+#
+#   keyval[1, ] <- .format_title(keyval[1, ])
+#
+#   return(keyval)
+# }
 
-  keyval[1, ] <- .format_title(keyval[1, ])
+.split_keyval <- function(x, only.first = T) {
+  y <- gsub(":[[:blank:]]+;?", "\t ", x)
+  keyval <- do.call(rbind, strsplit(y, split = "\t"))
+  keyval <- apply(keyval, 2, trimws)
+
+  keyval[, 1] <- .format_title(keyval[, 1])
 
   return(keyval)
+}
+
+.split_list <- function(x) {
+  # split list after colons
+  txt <- strsplit(x[2], "")[[1]]
+  colon <- head(which(txt == ":"), -1)
+  offset <- sapply(colon, function(x) grep("[^ ;]", tail(txt, -x))[1])
+  pos <- colon + offset
+  y <- do.call(rbind, lapply(x[-1], substring,
+                             first = c(1, pos),
+                             last = c(pos - 1, 10000L)))
+
+  tbl <- trimws(sub(";$", "", y[-1, , drop = F]))
+  colnames(tbl) <- .format_title(y[1, ])
+  tbl <- list(tbl)
+  names(tbl) <- .format_title(x[1])
+
+  return(tbl)
+}
+
+
+.split_list_whitespace <- function(x) {
+  x <- trimws(x)
+  y <- gsub(":?[[:blank:]]{3,};?", "\t", x)
+  y <- do.call(rbind, strsplit(tail(y, -1), split = "\t", fixed = TRUE))
+
+  tbl <- y[-1, , drop = F]
+  colnames(tbl) <- .format_title(y[1, ])
+  tbl <- list(tbl)
+  names(tbl) <- .format_title(x[1])
+
+  return(tbl)
 }
 
 
@@ -96,6 +144,7 @@ read.hzb <- function(file, parse.header = TRUE) {
 }
 
 
+
 .format_list_fwf <- function(x) {
   title <- .format_title(x[1])
   widths <- c(27, 20, 25)
@@ -105,6 +154,7 @@ read.hzb <- function(file, parse.header = TRUE) {
                 last = cumsum(widths))
 
   tbl <- apply(do.call(rbind, tbl), 2, trimws)
+  tbl <- tbl[, apply(tbl != "", 2, any)]
 
   # table can be empty
   if (is.null(nrow(tbl))) {
@@ -125,7 +175,7 @@ read.hzb <- function(file, parse.header = TRUE) {
 
 .format_title <- function(x) {
   # remove trailing ":"
-  y <- sub(":$", "", trimws(x))
+  y <- sub(":[[:blank:]]*;?$", "", trimws(x))
 
   # remove everything inside round brackets
   y <- gsub("\\(.*?\\)", "", y)
@@ -135,19 +185,42 @@ read.hzb <- function(file, parse.header = TRUE) {
   return(trimws(y))
 }
 
+.value <- function(x, name) x[x[, 1] == name, 2]
+.listvalue <- function(x, lname, ename) {
+  y <- x[[lname]][, ename, drop = F]
+  y <- if(nrow(y) < 1) NA else tail(y, 1)
+
+  return(y)
+}
+
 
 extract_meta <- function(x) {
-  .value <- function(x, name) x[x[, 1] == name, 2]
 
   att1 <- attr(x, "keyval")
   att2 <- attr(x, "list")
-  data.frame(name = .value(att1, "Messstelle"),
-             id.hzb = as.numeric(.value(att1, "HZB-Nummer")),
-             departement = .value(att1, "Dienststelle"),
-             lon = dms2deg(.value(att2[["Geographische Koordinaten"]], "Länge")),
-             lat = dms2deg(.value(att2[["Geographische Koordinaten"]], "Breite")),
-             z = as.numeric(att2[[grep("Höhenangaben", names(att2))]][1, "Geländehöhe"]))
+
+  y <- data.frame(name = .value(att1, "Messstelle"),
+                  id.hzb = as.numeric(.value(att1, "HZB-Nummer")),
+                  departement = .value(att1, "Dienststelle"),
+                  lon = dms2dec(.listvalue(att2, "Geographische Koordinaten", "L\u00e4nge")),
+                  lat = dms2dec(.listvalue(att2, "Geographische Koordinaten", "Breite")),
+                  z = as.numeric(sub(",", ".", .listvalue(att2, "Pegelnullpunkt", "H\u00f6he"))))
+
+  rownames(y) <- NULL
+  return(y)
 }
+
+extract_meta_xts <- function(x) {
+  att <- xtsAttributes(x)
+  meta <- att[setdiff(names(att), "coordinates")]
+  coord <- att[["coordinates"]]
+  meta[names(coord)] <- coord
+
+  names(meta)[names(meta) == "station"] <- "name"
+
+  return(as.data.frame(meta))
+}
+
 
 
 
@@ -158,8 +231,8 @@ nlast <- function(x, n = 1, col) {
 }
 
 
-regularize <- function(x) {
-  fullseq <- seq(from = min(time(x)), to = max(time(x)), by = "day")
+regularize <- function(x, interval = "day") {
+  fullseq <- seq(from = min(time(x)), to = max(time(x)), by = interval)
   missing <- fullseq[!fullseq %in% time(x)]
 
   if(length(missing)) {
@@ -171,6 +244,7 @@ regularize <- function(x) {
 }
 
 
+
 hzb2xts <- function(x){
   x$time <- as.Date(format(x$time, format = "%Y-%m-%d"))
   x <- x[!duplicated(x$time), ]
@@ -178,17 +252,40 @@ hzb2xts <- function(x){
   y <- regularize(y)
 
   att <- attr(x, "keyval")
-  coord <- dms2dec(nlast(attr(x, "list")$"Geographische Koordinaten")[2:3])
-  names(coord) <- c("long", "lat")
+  coord <- numeric()
+  coord[c("lon", "lat")] <- dms2dec(
+    .listvalue(attr(x, "list"), "Geographische Koordinaten",
+               c("L\u00e4nge", "Breite")))
 
-  xtsAttributes(y) <- list(id.hzb = as.numeric(att[att[, 1] == "HZB-Nummer", 2]),
-                           station = att[att[, 1] == "Messstelle", 2],
-                           river = att[att[, 1] == "Gewässer", 2],
-                           institution = att[att[, 1] == "Dienststelle", 2],
-                           catchment = att[att[, 1] == "orogr.Einzugsgebiet [km²]", 2],
-                           unit = sub("\u00B3", "^3", att[att[, 1] == "Einheit", 2]),
-                           coordinates = coord,
-                           z = as.numeric(nlast(attr(x, "list")$Pegelnullpunkt)[2]))
+  z <- as.numeric(.listvalue(attr(x, "list"), "Pegelnullpunkt", "H\u00f6he"))
+
+  dict <- c("Messstelle"="station", "HZB-Nummer"="id.hzb", "HD-Nummer"="id.hd",
+            "DBMS-Nummer"="id.dbms", "Gew\u00e4sser"="river",
+            "Messstellenbetreiber" = "operator", "Dienststelle" = "departement",
+            "orogr.Einzugsgebiet"="catchment", "Einheit"="unit")
+
+  keyval <- list()
+  for(i in seq_along(dict)) keyval[[i]] <- .value(att, names(dict)[i])
+  names(keyval) <- dict
+
+  keyval[["id.hzb"]] <- as.numeric(keyval[["id.hzb"]])
+  keyval[["catchment"]] <- as.numeric(keyval[["catchment"]])
+  keyval[["unit"]] <- sub("\u00B3", "^3",keyval[["unit"]])
+
+  xtsAttributes(y) <- c(keyval, list(coordinates = coord, z = z))
 
   return(y)
+}
+
+dms2dec <- function(x, split = " ", ...){
+  res <- rep(NA, length(x))
+
+  if(!all(is.na(x))) {
+    y <- strsplit(x[!is.na(x)], split = split, ...)
+    z <- sapply(y, function(x)
+      sum(as.numeric(x) * c(1, 1/60^seq_len(length(x) - 1))))
+    res[!is.na(x)] <- z
+  }
+
+  return(res)
 }
